c1ec490f01c01cc8efafd28f843f8df0a9bbc78d
Basil <basildev.biz@gmail.com>
2025-01-01 11:07:28 +0300

    frontend

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-     return render_template('/templates/index.html')
+     return render_template('index.html')
 
 @app.route('/register', methods=['POST'])
 def register():
